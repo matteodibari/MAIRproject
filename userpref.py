47f6d5994e081c0af60f32508e3658ae6994030f
matteodibari <mat.dibari@gmail.com>
2023-09-21 17:04:26 +0200

    userpref update (alex's version)

--- userpref.py
+++ userpref.py
@@ -1,6 +1,5 @@
 import tensorflow as tf
 import keras
-import csv
 import numpy as np
 import sklearn as sk
 from sklearn import model_selection
@@ -9,6 +8,8 @@ from keras import layers
 
 # import the enchant module
 import enchant
+import math
+
 #from enchant import utils
 
 # determining the values of the parameters
@@ -19,16 +20,24 @@ string2 = "Hello d"
 # string1 and string2
 print(enchant.utils.levenshtein(string1, string2))
 
+
 restaurantTypes = ["british", "european", "italian", "romanian", "seafood", "chinese", "steakhouse", "moderneuropean", "french", "asian", "portuguese", "indian", "spanish", "vietnamese", "european", "korean", "thai", "moroccan", "swiss", "gastropub", "fusion", "tuscan", "international", "traditional", "polynesian", "turkish", "african", "mediterranian", "bistro", "northamerican", "australian", "persian", "jamaikan", "lebanese", "cuban", "japanese", "catalan"]
 priceTypes = ["expensive", "moderate", "cheap"]
 locationTypes = ["south", "west", "centre", "east", "north"]
-sentense1 = "Im looking for an Expensive restaurant that serves asianoriental"
+
+
+dontCareLoc = ["anyplace", "anypart", "anyarea"]
+dontCarePrice = ["anyprice"]
+dontCareRest = ["anyfood", "anykind", "anytype"]
+sentense1 = "Im looking for an Expensive restaurant that serves asianoriental and north yes"
 sentense2 = "pan asian"
 sentense3 = "m o d e r a  t ly priced restaurant in the south part of town"
+sentense4 = "I'm looking for a restaurant in any area that serves Tuscan food"
 
 def compareSets(set1, set2):
     for type in set1:
         index = - 1
+        allPref= []
         for letter in set2:
             #print(letter)
             index = index + 1
@@ -36,32 +45,64 @@ def compareSets(set1, set2):
                 sub = set2[index: index + len(type)]
 
                 diff = enchant.utils.levenshtein(type, sub)
-                if diff < 2:
+                if diff < math.floor(len(type) / 5):
                     if type == 'asian':
                         type = 'asian oriental'
+                    return type
                     print(sub, type)
+                    #allPref.append(type)
+    #return allPref
 
 def getUserPref(sentence):
     sentence = sentence.replace(" ", "")
     sentence = sentence.lower()
+    allPref = []
     #print(sentence)
-    compareSets(priceTypes, sentence)
-    compareSets(locationTypes, sentence)
-    compareSets(restaurantTypes, sentence)
+    allPref.append(compareSets(priceTypes, sentence))
+    allPref.append(compareSets(locationTypes, sentence))
+    allPref.append(compareSets(restaurantTypes, sentence))
+    if allPref[0] is None:
+        print('one')
+        if compareSets(dontCarePrice, sentence) is not None:
+            allPref[0] = "any"
+    if allPref[1] is None:
+        print('two')
+        if compareSets(dontCareLoc, sentence) is not None:
+            allPref[1] = "any"
+    if allPref[2] is None:
+        if compareSets(dontCareRest, sentence) is not None:
+            allPref[2] = "any"
+    return allPref
 
-getUserPref(sentense1)
-getUserPref(sentense2)
-getUserPref(sentense3)
 
 
+print(getUserPref(sentense4))
+#getUserPref(sentense2)
+#getUserPref(sentense3)
 
-with open('restaurant_info.csv', newline='') as csvfile:
-    spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
-    for row in spamreader:
-        print(row)
+i = 0
+import csv
+if "area" in ['"restaurantname"', '"pricerange"', '"area"', '"food"', '"phone"', '"addr"', '"postcode"']:
+    print('da')
+if '"area"' in ['"restaurantname"', '"pricerange"', '"area"', '"food"', '"phone"', '"addr"', '"postcode"']:
+    print('da')
+#if ('\"' + pref + '\"') in ['"pizza express fen ditton"', '"moderate"', '"centre"', '"european"', '""', '"jesus lane fen ditton"', '""']:
 
+def recommend(allPref):
+    print(allPref)
+    with open('restaurant_info.csv', newline='') as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=',', quotechar='\"')
 
-while(1):
-    input_msg = input()
-    output_msg = ''
-    print(output_msg)
+        for row in spamreader:
+            #print(row)
+            score = 0
+            for pref in allPref:
+                #print(pref, row)
+                if pref in row:
+                    #print('hi')
+                    score = score + 1
+            if score == 3:
+                print('yes')
+                print(row)
+recommend(getUserPref(sentense1))
+#recommend(sentense3)
