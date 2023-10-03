323dc2d784c8ce2a9da011433d91ba126d840432
matteodibari <mat.dibari@gmail.com>
2023-10-03 15:19:42 +0200

    03/10 UPDATES

--- generate_response.py
+++ generate_response.py
@@ -5,6 +5,7 @@ from model_feed_forward import vector_to_label
 from model_feed_forward import to_one_hot
 from userpref import getUserPref
 from userpref import recommend
+from userpref import getUserRequest
 
 
 curr_state = 1
@@ -12,26 +13,13 @@ curr_state = 1
 #stored-data = [price-range, area, type]
 stored_preferences = [None, None, None]
 
-apologies = ['Sorry, I did not find any restaurant matching these preferences.']
 ask_area = ['In which area are you searching for the restaurant?']
 ask_type = ['Which type of food are you searching for?']
 bye = ['Bye man!']
 error = ['Sorry, I did not understand what you meant.']
 
-model = load_model('saved_model.h5')
-words = None
-with open('bag_of_words.txt', 'r') as f:
-    data = f.read()
-    words = data.split('\n')
 
-y = model.predict( np.array( [sentence_to_vector_2('what is happening', words)] ))
-y = to_one_hot(y[0])
-print(y)
-print(vector_to_label(y))
-
-
-
-#print('Hello , welcome to the Cambridge restaurant system? You can ask for restaurants by area , price range or food type . How may I help you?')
+print('Hello , welcome to the Cambridge restaurant system? You can ask for restaurants by area , price range or food type . How may I help you?')
 
 def print_restaurant(restaurant_info):
     print('The perfect restaurant is ' + restaurant_info[0] + ' with ' + restaurant_info[1] + 
@@ -39,128 +27,154 @@ def print_restaurant(restaurant_info):
                           + restaurant_info[3] + ' food')
     return
 
+def predict_dialog_act(input_msg):
+    model = load_model('saved_model.h5')
+    words = None
+    with open('bag_of_words.txt', 'r') as f:
+        data = f.read()
+        words = data.split('\n')
 
-# while(1):
-#     user_input = input()
-#     dialog_act = 'thankyou'
+    y = model.predict( np.array( [sentence_to_vector_2(input_msg, words),] ))
+    y = to_one_hot(y[0])
+    return vector_to_label(y)
 
-#     preferences = getUserPref(user_input)
-#     # preferences[3] = [price, location, typeoffood]
-
-#     for i in range(len(stored_preferences)):
-#         if stored_preferences[i] == None: stored_preferences[i] = preferences[i]
-
-#     print(stored_preferences)
+def print_apologies():
+    print(f'Sorry, I did not find any restaurant matching these preferences: {stored_preferences[0]}, {stored_preferences[1]}, {stored_preferences[2]}. Please search again.')
 
 
-#     match curr_state:
-#         case 1:
-#             print('sys just entered state 1')
-#             if stored_preferences[1] == None: 
-#                 print(ask_area[0])
-#                 curr_state = 2
-#             elif stored_preferences[2] == None:
-#                 print(ask_type[0])
-#                 curr_state = 3
-#             else:
-#                 restaurant_info = recommend(stored_preferences)
-#                 #restaurant_info[4] = [restaurant, price, location, typeoffood]
-                
-#                 if restaurant_info:
-#                     curr_state = 4
-#                     print_restaurant(restaurant_info)
-#                 else:
-#                     curr_state = 5
-#                     print(apologies[0])
-#         case 2:
-#             print('sys just entered state 2')
-#             if dialog_act == 'None':
-#                 curr_state = 2
-#                 continue
+#list of already recommended restaurant (list of arrays)
+already_recommended = []
 
-#             if stored_preferences[2] == None:
-#                 print(ask_type[0])
-#                 curr_state = 3
-#             else:
-#                 restaurant_info = recommend(stored_preferences)
-#                 if restaurant_info:
-#                     curr_state = 4
-#                     print_restaurant(restaurant_info)
-#                 else:
-#                     curr_state = 5
-#                     print(apologies[0])
 
-#         case 3:
-#             print('SYSTEM: just entered state 3')
-#             if dialog_act == 'None':
-#                 curr_state = 3
-#                 continue
+while(1):
+    user_input = input()
+    dialog_act = predict_dialog_act(user_input)
+    print(dialog_act)
+    preferences_changed = False
+
+    #clearing the preferences if we entered in state 5
+    if curr_state == 5: 
+        stored_preferences = [None, None, None]
+
+    preferences = getUserPref(user_input)
+    # preferences[3] = [price, location, typeoffood]
+
+    for i in range(len(stored_preferences)):
+        if preferences[i] != None: 
+            stored_preferences[i] = preferences[i]
+            preferences_changed = True
+
+
+    print(stored_preferences)
+
+
+    match curr_state:
+        case 1:
             
-#             restaurant_info = recommend(stored_preferences)
-#             print(restaurant_info)
+            if stored_preferences[1] == None: 
+                print(ask_area[0])
+                curr_state = 2
+            elif stored_preferences[2] == None:
+                print(ask_type[0])
+                curr_state = 3
+            else:
+                restaurant_info = recommend(stored_preferences, already_recommended)
+                #restaurant_info[4] = [restaurant, price, location, typeoffood]
+                
+                if restaurant_info:
+                    already_recommended.append(restaurant_info)
+                    curr_state = 4
+                    print_restaurant(restaurant_info)
+                else:
+                    curr_state = 5
+                    print_apologies()
+        case 2:
+            
+            if dialog_act == 'None':
+                curr_state = 2
+                continue
 
-#             if restaurant_info:
-#                 curr_state = 4
-#                 print_restaurant(restaurant_info)
-#             else:
-#                 curr_state = 5
-#                 print(apologies[0])
+            if stored_preferences[2] == None:
+                print(ask_type[0])
+                curr_state = 3
+            else:
+                restaurant_info = recommend(stored_preferences, already_recommended)
+                
+                if restaurant_info:
+                    already_recommended.append(restaurant_info)
+                    curr_state = 4
+                    print_restaurant(restaurant_info)
+                else:
+                    curr_state = 5
+                    print_apologies()
 
-#         case 4:
-#             if dialog_act == 'reqalts':
-#                 restaurant_info = recommend(stored_preferences)
-#                 if restaurant_info:
-#                     curr_state = 4
-#                     print_restaurant(restaurant_info)
-#                 else:
-#                     curr_state = 5
-#                     print(apologies[0])
+        case 3:
+            
+            if dialog_act == 'None':
+                curr_state = 3
+                continue
+            
+            restaurant_info = recommend(stored_preferences, already_recommended)
 
-#             elif dialog_act in ['request', 'reqmore']:
-#                 # search for the infos
-#                 # give the info
-#                 curr_state = 7
+            if restaurant_info:
+                already_recommended.append(restaurant_info)
+                curr_state = 4
+                print_restaurant(restaurant_info)
+            else:
+                curr_state = 5
+                print_apologies()
 
-#             elif dialog_act == 'thankyou':
-#                 print(bye[0])
-#                 exit()
+        case 4:
 
-#             else:
-#                 if dialog_act == 'repeat':
-#                     continue
-#                 else:
-#                     print(error[0])          
+            if dialog_act in ['reqalts', 'request', 'reqmore', 'inform']:
+                if preferences_changed:
+                    restaurant_info = recommend(stored_preferences, already_recommended)
+                    if restaurant_info:
+                        already_recommended.append(restaurant_info)
+                        curr_state = 4
+                        print_restaurant(restaurant_info)
+                    else:
+                        curr_state = 5
+                        print_apologies()
+                else:
+                    requests = getUserRequest(user_input)
+                    if requests[0] != None: print(f'The phone number of the restaurant is {already_recommended[-1][4]}')
+                    if requests[1] != None: print(f'The address of the restaurant is {already_recommended[-1][5]}')
+                    if requests[2] != None: print(f'The post code of the restaurant is {already_recommended[-1][6]}')
+                    curr_state = 4
+            elif dialog_act in ['thankyou', 'bye']:
+                print(bye[0])
+                exit()
+            else:
+                print(error[0])  
 
-#         case 5:
-#             if stored_preferences[0] == None: 
-#                 curr_state = 2
-#             elif stored_preferences[1] == None:
-#                 curr_state = 3
-#             else:
-#                 restaurant_info = recommend(stored_preferences)
-#                 if restaurant_info:
-#                     curr_state = 4
-#                     print_restaurant(restaurant_info)
-#                 else:
-#                     curr_state = 5
-#                     print(apologies[0])
+        case 5:
+
+            if dialog_act in ['thankyou', 'bye']:
+                print(bye[0])
+                exit()
+
+            if stored_preferences[1] == None: 
+                print(ask_area[0])
+                curr_state = 2
+            elif stored_preferences[2] == None:
+                print(ask_type[0])
+                curr_state = 3
+            else:
+                restaurant_info = recommend(stored_preferences, already_recommended)
+                #restaurant_info[4] = [restaurant, price, location, typeoffood]
+                
+                if restaurant_info:
+                    already_recommended.append(restaurant_info[0])
+                    curr_state = 4
+                    print_restaurant(restaurant_info)
+                else:
+                    curr_state = 5
+                    print_apologies()
+        
+
+    print(curr_state)
 
 
 
 
-def generate_response(input_msg, curr_state):
-    #define the states
-
-    #categorize the input sentence
-    # model = load_model('saved_model.h5')
-
-    #read the bag of words from the file
-    # words = None
-    # with open('bag_of_words.txt', 'r') as f:
-    #     data = f.read()
-    #     words = data.split('\n')
-
-    # y = model.predict( np.array( [sentence_to_vector_2(input_msg, words),] ))
-    # print(vector_to_label(y[0]))
-    pass
-  
--- userpref.py
+++ userpref.py
@@ -9,15 +9,7 @@ from keras import layers
 # import the enchant module
 from enchant import utils
 import math
-
-#from enchant import utils
-
-# determining the values of the parameters
-string1 = "Hello World"
-string2 = "Hello d"
-
-# the Levenshtein distance between
-# string1 and string2
+import csv
 
 
 restaurantTypes = ["british", "european", "italian", "romanian", "seafood", "chinese", "steakhouse", "moderneuropean", "french", "asian", "portuguese", "indian", "spanish", "vietnamese", "european", "korean", "thai", "moroccan", "swiss", "gastropub", "fusion", "tuscan", "international", "traditional", "polynesian", "turkish", "african", "mediterranian", "bistro", "northamerican", "australian", "persian", "jamaikan", "lebanese", "cuban", "japanese", "catalan"]
@@ -28,37 +20,64 @@ locationTypes = ["south", "west", "centre", "east", "north"]
 dontCareLoc = ["anyplace", "anypart", "anyarea"]
 dontCarePrice = ["anyprice"]
 dontCareRest = ["anyfood", "anykind", "anytype"]
+
+postCodeKeyWords = ["postalcode", "postcode"]
+phoneKeyWords = ["contact", "phone"]
+addressKeyWords= ["address", "located"]
+
+
 sentense1 = "Im looking for an Expensive restaurant that serves asianoriental and north yes"
-sentense2 = "pan asian west"
+sentense2 = "pan asian north "
 sentense3 = "m o d e r a  t ly priced restaurant in the south part of town"
 sentense4 = "I'm looking for a restaurant in any area that serves Tuscan food"
+sentense5 = "I would like the restaurant to be romantic"
+sentense6 = "uh whats the phone number and address please"
 
-def compareSets(set1, set2):
-    for type in set1:
+
+
+def compareSets(types, sentence):
+    """
+    This function take two arrrays. First one will be possible types of location price or food
+    and the second one will be the sentence. The function check levenshtein distance  of each type
+    on every subarray with the same length of it from the sentence and return the firrst match.
+    The distance should be less or equal to the length of the word so we can still catch wrong writen
+    longer words and also keep the accuracy good for
+
+    :param set1: First set to compare
+    :param sentence: Second set to comapare
+    :return: The first instance of an element in set
+             one that has small enough levenshtein distance with eleement in set two.
+    """
+    for type in types:
         index = - 1
-        allPref= []
-        for letter in set2:
-            #print(letter)
+        for letter in sentence:
             index = index + 1
-            if letter == type[0] and index + len(type) < len(set2) + 1:
-                sub = set2[index: index + len(type)]
+            if letter == type[0] and index + len(type) < len(sentence) + 1:
+                sub = sentence[index: index + len(type)]
 
                 diff = utils.levenshtein(type, sub)
-                if diff < math.floor(len(type) / 5):
+                if diff <= math.floor(len(type) / 5):
                     if type == 'asian':
                         type = 'asian oriental'
                     return type
-                    #allPref.append(type)
-    #return allPref
+
 
 def getUserPref(sentence):
+    """
+    This function check the sentence for all keywords about type of food, location and price.
+    Then matches keywords for dont care and fill them ontho the needed places.
+
+    :param sentence: The sentence in which we weill search preferences
+    :return: list with preferences for price, location and food
+    """
     sentence = sentence.replace(" ", "")
     sentence = sentence.lower()
+
     allPref = []
-    #print(sentence)
     allPref.append(compareSets(priceTypes, sentence))
     allPref.append(compareSets(locationTypes, sentence))
     allPref.append(compareSets(restaurantTypes, sentence))
+
     if allPref[0] is None:
         if compareSets(dontCarePrice, sentence) is not None:
             allPref[0] = "any"
@@ -70,57 +89,70 @@ def getUserPref(sentence):
             allPref[2] = "any"
     return allPref
 
+def getUserRequest(sentence):
+    sentence = sentence.replace(" ", "")
+    sentence = sentence.lower()
 
+    allPref = []
+    allPref.append(compareSets(phoneKeyWords, sentence))
+    allPref.append(compareSets(addressKeyWords, sentence))
+    allPref.append(compareSets(postCodeKeyWords, sentence))
 
-print(getUserPref(sentense2))
-#getUserPref(sentense2)
-#getUserPref(sentense3)
+    return allPref
 
-i = 0
-import csv
-if "area" in ['"restaurantname"', '"pricerange"', '"area"', '"food"', '"phone"', '"addr"', '"postcode"']:
-    print('da')
-if '"area"' in ['"restaurantname"', '"pricerange"', '"area"', '"food"', '"phone"', '"addr"', '"postcode"']:
-    print('da')
-#if ('\"' + pref + '\"') in ['"pizza express fen ditton"', '"moderate"', '"centre"', '"european"', '""', '"jesus lane fen ditton"', '""']:
-
-def recommend(allPref):
+def recommend(allPref, already_recommended, all=False):
     matchesNeeded = 3
     userPref = []
     for pref in allPref:
         if pref == 'any' or pref is None:
             matchesNeeded = matchesNeeded - 1
+    allRestaurants = []
 
     with open('restaurant_info.csv', newline='') as csvfile:
         spamreader = csv.reader(csvfile, delimiter=',', quotechar='\"')
+
         for row in spamreader:
-            #print(row)
             score = 0
             for pref in allPref:
-                #print(pref, row)
                 if pref in row:
-                    #print('hi')
                     score = score + 1
-            if score == matchesNeeded:
+                if pref == 'asian oriental' and 'asian' in row:
+                    score = score + 1
+            if score == matchesNeeded and row not in already_recommended[:]:            
                 userPref.append(row[0])
-                userPref.append(row[1])
-                userPref.append(row[2])
-                userPref.append(row[3])
-                # for i in range(3):
-                #     if allPref[i] == 'any':
-                #         userPref.append('any')
-                #     elif allPref[i] is None:
-                #         userPref.append("unknown")
-                #     else:
-                #         userPref.append(row[i + 1])
-                break
+                for i in range(3):
+                    if allPref[i] == 'any':
+                        userPref.append('any')
+                    elif allPref[i] is None:
+                        userPref.append("unknown")
+                    else:
+                        userPref.append(row[i + 1])
+                userPref.extend(row[4:7])
+                if all == False:
+                    break
+                allRestaurants.append(row[0])
+                
+    if all == False:
+        return userPref
+    return allRestaurants
 
-    return userPref
+keyWords = ['touristic', 'assignedseats', 'children', 'romantic']
 
+def aditionalInfoRecommend(sentence, lastSentence):
+    posibleRestaurants = recommend(getUserPref(lastSentence), True)
+    userpref = compareSets(keyWords, sentence)
 
-# print(recommend(getUserPref(sentense1)))
-# print(recommend(getUserPref(sentense4)))
-# pref = recommend(getUserPref(sentense1))
-# print('The perfect restaurant is ' + pref[0] + ' with ' + pref[1] + ' price range which is located in ' + pref [2] + ' part of the town and serve ' + pref[3] + ' food')
-# pref = recommend(getUserPref(sentense4))
-# print('The perfect restaurant is ' + pref[0] + ' with ' + pref[1] + ' price range which is located in ' + pref [2] + ' part of the town and serve ' + pref[3] + ' food')
+    with open('restaurant_info.csv', newline='') as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=',', quotechar='\"')
+
+        for row in spamreader:
+            if len(row) < 1:
+                continue
+            if userpref == 'turistc' and row[1] == 'cheap' and row[7] == 'good' and row[2] != 'romanian' and row[0] in posibleRestaurants:
+                return row
+            if userpref == 'assignedseats' and row[8] == 'busy' and row[0] in posibleRestaurants:
+                return row
+            if userpref == 'children' and row[9] != 'long time' and row[0] in posibleRestaurants:
+                return row
+            if userpref == 'romantic' and row[8] != 'busy' and row[9] == 'long time' and row[0] in posibleRestaurants:
+                return row
