481fca68a15debf1f63b57840524706f6c54a590
alexs63 <57115982+alexs63@users.noreply.github.com>
2023-10-07 12:33:48 +0300

    Update user_pref.py

--- user_pref.py
+++ user_pref.py
@@ -19,32 +19,33 @@ address_keywords= ["address", "located"]
 
 additional_infos = ['touristic', 'assignedseats', 'children', 'romantic']
 
-def compare_sets(keywords, sentence):
+def compare_sets(keywords, sentence, precision):
     """
     This function uses Levenshtein distance to find keyword matches inside a sentence.
 
     :param types: keywords.
     :param sentence: sentence to use for the matching.
-    :return keyword: first matching keyword. 
+    :return keyword: first matching keyword.
     """
     for keyword in keywords:
         index = 0
         for letter in sentence:
-            #checking the first letter that matches the first letter of the keyword and
-            #checking if the keyword fits the rest of the sentence
+            # checking the first letter that matches the first letter of the keyword and
+            # checking if the keyword fits the rest of the sentence
             if letter == keyword[0] and index + len(keyword) < len(sentence) + 1:
                 sub = sentence[index: index + len(keyword)]
                 dinstance = utils.levenshtein(keyword, sub)
-                if dinstance <= math.floor(len(keyword) / 5):
+                if dinstance <= math.floor(len(keyword) / precision):
                     if keyword == 'asian':
                         keyword = 'asian oriental'
                     return keyword
             index += 1
 
-def get_user_pref(sentence):
+
+def get_user_pref(sentence, precision):
     """
     This function checks for any keywords in a sentence using the compare_sets function.
-    Moreover it checks for the user does not have a preference, in which case it fills 
+    Moreover it checks for the user does not have a preference, in which case it fills
     the corresponding field with 'any'.
 
     :param sentence: sentence to use for the keyword matching.
@@ -54,22 +55,23 @@ def get_user_pref(sentence):
     sentence = sentence.lower()
 
     all_pref = []
-    all_pref.append(compare_sets(price_types, sentence))
-    all_pref.append(compare_sets(location_types, sentence))
-    all_pref.append(compare_sets(restaurant_types, sentence))
+    all_pref.append(compare_sets(price_types, sentence, precision))
+    all_pref.append(compare_sets(location_types, sentence, precision))
+    all_pref.append(compare_sets(restaurant_types, sentence, precision))
 
     if all_pref[0] is None:
-        if compare_sets(dont_care_price, sentence) is not None:
+        if compare_sets(dont_care_price, sentence, precision) is not None:
             all_pref[0] = "any"
     if all_pref[1] is None:
-        if compare_sets(dont_care_loc, sentence) is not None:
+        if compare_sets(dont_care_loc, sentence, precision) is not None:
             all_pref[1] = "any"
     if all_pref[2] is None:
-        if compare_sets(dont_care_rest, sentence) is not None:
+        if compare_sets(dont_care_rest, sentence, precision) is not None:
             all_pref[2] = "any"
     return all_pref
 
-def get_user_request(sentence):
+
+def get_user_request(sentence, precision):
     """
     A function that checks for requests about phone number, post code or address.
 
@@ -80,25 +82,26 @@ def get_user_request(sentence):
     sentence = sentence.lower()
 
     all_pref = []
-    all_pref.append(compare_sets(phone_keywords, sentence))
-    all_pref.append(compare_sets(address_keywords, sentence))
-    all_pref.append(compare_sets(post_code_keywords, sentence))
+    all_pref.append(compare_sets(phone_keywords, sentence, precision))
+    all_pref.append(compare_sets(address_keywords, sentence, precision))
+    all_pref.append(compare_sets(post_code_keywords, sentence, precision))
 
     return all_pref
 
-def recommend(all_pref, already_recommended, all=False):
+
+def recommend(all_pref, already_recommended, randomOutput, all=False):
     """
     The function finds the best possible restaurant, based on the preferences of the user
     and the additional preferences that are asked separately. First, the function finds the
     number of needed matches for requirements which removes 'any's and 'None's. Then reads
     all the restaurants from the database and checks for the number of matches.
-    
+
     :param all_pref: all user preferences.
     :param already_recommended: list with all already recommended restaurants.
     :param all: if this param is true the function returns a list of all the possible restaurants.
     :return restaurant: the whole line of the database relative to the recommended restaurant.
     """
-    randomOutput = input("Do you want the preferences to be in random order")
+
     possible_restaurants, additional_request = recommend_additional_info()
     matches_needed = 3
     user_pref = []
@@ -136,15 +139,15 @@ def recommend(all_pref, already_recommended, all=False):
 
     if all == False:
         if randomOutput == 'yes':
-            index = random.randint(0, len(allRestaurants) - 1)
-            allRestaurants[index].append(additionalRequest)
-            return allRestaurants[index]
+            index = random.randint(0, len(all_restaurants) - 1)
+            all_restaurants[index].append(additional_request)
+            return all_restaurants[index]
         user_pref.append(additional_request)
         return user_pref
     return all_restaurants
 
 
-def check_dont_care(sentence):
+def check_dont_care(sentence, precision):
     """
     This function checks for any dont care statements in a sentence.
 
@@ -154,12 +157,13 @@ def check_dont_care(sentence):
 
     sentence = sentence.replace(" ", "")
     sentence = sentence.lower()
-    if compare_sets(['dontcare', 'doesntmatter', 'doesnotmatter', 'donotcare', 'nopreference'], sentence):
+    if compare_sets(['dontcare', 'doesntmatter', 'doesnotmatter', 'donotcare', 'nopreference'], sentence, precision):
         return True
     else:
         return False
 
-def recommend_additional_info():
+
+def recommend_additional_info(precision=5):
     """
     This function takes a sentence from standard input and it searches for
     preferences about the place (good for children, romantic, etc.) and gives a
@@ -167,14 +171,14 @@ def recommend_additional_info():
 
     :return: list of possible restaurants and users preferences.
     """
-    
+
     sentence = input("Do you have additional requirements?\n")
 
     if sentence == "No":
         return None, None
 
     possible_restaurants = []
-    userpref = compare_sets(additional_infos, sentence)
+    userpref = compare_sets(additional_infos, sentence, precision)
 
     with open('restaurant_info.csv', newline='') as csvfile:
         spamreader = csv.reader(csvfile, delimiter=',', quotechar='\"')
@@ -193,5 +197,5 @@ def recommend_additional_info():
     if len(possible_restaurants) == 0:
         possible_restaurants = None
         userpref = None
-    
+
     return possible_restaurants, userpref
